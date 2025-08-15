4d595ff1eb0534059bac8d505c9381b8615ab614
OsamaElsaadany <111007719+OsamaElsaadany@users.noreply.github.com>
2025-08-15 18:42:54 +0300

    Topicsearch

---
+++ lib/TopicSearch.dart
@@ -0,0 +1,19 @@
+//Stateless wedgit is the widget that never changes
+//it is used to display static content
+//it must extends class stateless widget
+//it is built only once
+
+//============================
+
+//stateful wedgit is the widget that can change
+//it is used to display dynamic content 
+//it must extends class stateful widget
+//setstate() is used to update the state of the widget
+//it is built multiple times
+//it can expressed in two classes
+//1. the widget class that extends StatefulWidget
+//it is the wedgit itself
+//2. the state class that extends State<WidgetClass>
+//it is the state of the widget that will be updated
+
+
